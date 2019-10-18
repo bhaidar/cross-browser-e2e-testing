62e38fbb53552be941b74e194940ea05a0e78344
Bilal Haidar <bhaidar@gmail.com>
2019-10-18 12:08:55 +0300

    convert E2E test to a visual one

--- cypress/integration/applitools-search.spec.js
+++ cypress/integration/applitools-search.spec.js
@@ -2,10 +2,29 @@
 
 context('Applitools', () => {
     beforeEach(() => {
+
+        // Open the Applitools Test
+        cy.eyesOpen({
+            appName: 'Cross Browser Testing',
+            browser: [
+                { width: 800, height: 600, name: 'firefox' },
+                { width: 1024, height: 768, name: 'chrome' },
+                { deviceName: 'iPhone X', screenOrientation: 'landscape', name: 'chrome' }
+            ]
+        });
+
         cy.visit('https://applitools.com')
+    });
+
+    afterEach(() => {
+        // Close the active test
+        cy.eyesClose();
     })
 
     it('search for visual grid', () => {
+
+        // Take a snapshot of the Applitools home page
+        cy.eyesCheckWindow('Main Page');
 
         // get and click the search icon
         cy.get('span.sb-search-icon.link').click();
@@ -15,6 +34,9 @@ context('Applitools', () => {
             .type('visual grid')
             .should('have.value', 'visual grid')
             .type('{enter}');
+
+        // Take a snapshot of the search results
+        cy.eyesCheckWindow('Search Results');
 
         // locate the 'Introduction to Visual Grid' link and click it
         cy.get('ul.ss360-list')
@@ -28,5 +50,8 @@ context('Applitools', () => {
             .find('h1')
             .should('be.visible')
             .and('contain.text', 'Introduction to the Visual Grid');
+
+        // Take a snapshot of the Introduction to the Visual Grid page
+        cy.eyesCheckWindow('Introduction to the Visual Grid page');
     });
 })
