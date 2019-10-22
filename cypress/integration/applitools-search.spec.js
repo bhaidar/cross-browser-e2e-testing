aa6d0bf6aafd51e6253d7e50238c7f3e5a23d2b7
Bilal Haidar <bhaidar@gmail.com>
2019-10-23 00:06:48 +0300

    make use of salesforce website

--- cypress/integration/applitools-search.spec.js
+++ cypress/integration/applitools-search.spec.js
@@ -13,7 +13,7 @@ context('Applitools', () => {
             ]
         });
 
-        cy.visit('https://applitools.com')
+        cy.visit('/form/contact/contactme')
     });
 
     afterEach(() => {
@@ -23,35 +23,91 @@ context('Applitools', () => {
 
     it('search for visual grid', () => {
 
-        // Take a snapshot of the Applitools home page
-        cy.eyesCheckWindow('Main Page');
+        // Wait on the page to load
+        cy.location('pathname', { timeout: 10000 })
+            .should('include', '/contactme');
 
-        // get and click the search icon
-        cy.get('span.sb-search-icon.link').click();
+        // Take a snapshot of the contact form
+        cy.eyesCheckWindow('Contact Form');
 
-        // type the text "visual grid" and hit Enter
-        cy.get('input#searchBox')
-            .type('visual grid')
-            .should('have.value', 'visual grid')
-            .type('{enter}');
+        // Fill First Name
+        cy.get('input[id="reg_form_1-UserFirstName"]')
+            .type('John')
+            .should('have.value', 'John');
 
-        // Take a snapshot of the search results
-        cy.eyesCheckWindow('Search Results');
+        // Fill Last Name
+        cy.get('input[id="reg_form_1-UserLastName"]')
+            .type('Smith')
+            .should('have.value', 'Smith');
 
-        // locate the 'Introduction to Visual Grid' link and click it
-        cy.get('ul.ss360-list')
-            .first()
-            .find('a')
-            .first()
-            .click({ force: true });
+        // Fill User Title
+        cy.get('input[id="reg_form_1-UserTitle"]')
+            .type('Senior Manager')
+            .should('have.value', 'Senior Manager');
 
-        // you are on the Visual Grid page, verify
-        cy.get('#document-body')
-            .find('h1')
-            .should('be.visible')
-            .and('contain.text', 'Introduction to the Visual Grid');
+        // Fill a wrong email address
+        cy.get('input[id="reg_form_1-UserEmail"]')
+            .type('john.smith2gmail.com')
+            .should('have.value', 'john.smith2gmail.com');
 
-        // Take a snapshot of the Introduction to the Visual Grid page
-        cy.eyesCheckWindow('Introduction to the Visual Grid page');
+        // Fill a Phone number
+        cy.get('input[id="reg_form_1-UserPhone"]')
+            .type('+1-202-555-0176')
+            .should('have.value', '+1-202-555-0176');
+
+        // Error msg for Email should now be shown
+        cy.get('#reg_form_1-UserEmail-errorMsg').should('be.visible', true);
+
+        // Take a snapshot of the contact form with email error message shown
+        cy.eyesCheckWindow('Contact Form with Email error message');
+
+        // Fill a valid email address
+        cy.get('input[id="reg_form_1-UserEmail"]')
+            .clear()
+            .type('john.smith@gmail.com')
+            .should('have.value', 'john.smith@gmail.com');
+
+        // Fill a Company Name
+        cy.get('input[id="reg_form_1-CompanyName"]')
+            .type('John Smith LTD')
+            .should('have.value', 'John Smith LTD');
+
+        // Select number of employees
+        cy.get('select[id="reg_form_1-CompanyEmployees"]')
+            .select('1 - 10 employees')
+            .should('have.value', '3');
+
+        // Select Country
+        cy.get('select[id="reg_form_1-CompanyCountry"]')
+            .select('United States')
+            .should('have.value', 'US');
+
+        // Select State
+        cy.get('select[id="reg_form_1-CompanyState"]')
+            .select('Arizona')
+            .should('have.value', 'AZ');
+
+        // Select Product/Interest
+        cy.get('select[id="reg_form_1-Lead.Primary_Product_Interest__c"]')
+            .select('Salesforce automation & CRM')
+            .should('have.value', 'Sales');
+
+        // Fill the User Description
+        cy.get('textarea[id="reg_form_1-UserDescription"]')
+            .type('I am requesting a demo!')
+            .should('have.value', 'I am requesting a demo!');
+
+        // Submit the form
+        cy.get('button[name="Submit"]').click();
+
+        // Wait on the page to load
+        cy.location('pathname', { timeout: 10000 })
+            .should('include', '/conf/contactme');
+
+        // Verify the message has went through
+        cy.get('h2').contains("We'll be in touch").should('be.visible', true);
+
+        // Take a snapshot of the contact form submission
+        cy.eyesCheckWindow('Contact Form Submission Results');
     });
 })
