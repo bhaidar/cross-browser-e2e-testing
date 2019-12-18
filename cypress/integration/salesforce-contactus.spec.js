2563ec57b0ed5a9a560c3391562babb598f2c655
Bilal Haidar <bhaidar@gmail.com>
2019-12-18 17:47:49 +0200

    update script

--- cypress/integration/salesforce-contactus.spec.js
+++ cypress/integration/salesforce-contactus.spec.js
@@ -34,57 +34,57 @@ context('Applitools', () => {
         cy.eyesCheckWindow('Contact Form');
 
         // Fill First Name
-        cy.get('input[id="reg_form_1-UserFirstName"]')
+        cy.get('input[id^="UserFirstName"]')
             .type('John')
             .should('have.value', 'John');
 
         // Fill Last Name
-        cy.get('input[id="reg_form_1-UserLastName"]')
+        cy.get('input[id^="UserLastName"]')
             .type('Smith')
             .should('have.value', 'Smith');
 
         // Fill User Title
-        cy.get('input[id="reg_form_1-UserTitle"]')
+        cy.get('input[id^="UserTitle"]')
             .type('Senior Manager')
             .should('have.value', 'Senior Manager');
 
         // Fill a email address
-        cy.get('input[id="reg_form_1-UserEmail"]')
+        cy.get('input[id^="UserEmail"]')
             .type('john.smith@gmail.com')
             .should('have.value', 'john.smith@gmail.com');
 
         // Fill a Phone number
-        cy.get('input[id="reg_form_1-UserPhone"]')
+        cy.get('input[id^="UserPhone"]')
             .type('+1-202-555-0176')
             .should('have.value', '+1-202-555-0176');
 
         // Fill a Company Name
-        cy.get('input[id="reg_form_1-CompanyName"]')
+        cy.get('input[id^="CompanyName"]')
             .type('John Smith LTD')
             .should('have.value', 'John Smith LTD');
 
         // Select number of employees
-        cy.get('select[id="reg_form_1-CompanyEmployees"]')
+        cy.get('select[id^="CompanyEmployees"]')
             .select('3')
             .should('have.value', '3');
 
         // Select Country
-        cy.get('select[id="reg_form_1-CompanyCountry"]')
+        cy.get('select[id^="CompanyCountry"]')
             .select('US')
             .should('have.value', 'US');
 
         // Select State
-        cy.get('select[id="reg_form_1-CompanyState"]')
+        cy.get('select[id^="CompanyState"]')
             .select('AZ')
             .should('have.value', 'AZ');
 
         // Select Product/Interest
-        cy.get('select[id="reg_form_1-Lead.Primary_Product_Interest__c"]')
+        cy.get('select[id^="Lead.Primary_Product_Interest"]')
             .select('Sales')
             .should('have.value', 'Sales');
 
         // Fill the User Description
-        cy.get('textarea[id="reg_form_1-UserDescription"]')
+        cy.get('textarea[id^="UserDescription"]')
             .type('I am requesting a demo!')
             .blur()
             .should('have.value', 'I am requesting a demo!');
